031ad89831d4fd0de7d5a726c8c29ecc0b81c3ce
kyledavid <kyledavid022@gmail.com>
2017-08-24 22:28:02 +0000

    we have rows!

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,10 +1,63 @@
 import React, {Component} from 'react'
+import {Stage} from 'react-konva'
+import {Board} from '../styled/TicTacToe'
 
 class TicTacToe extends Component {
+    
+    state = {
+        rows: 3,
+    }
+    
+    componentWillMount() {
+        let height = window.innerHeight
+        let width = window.innerWidth
+        let size = (height < width) ? height * .8 : width * .8
+        let rows = this.state.rows
+        let unit = size / rows
+        
+        this.setState({
+            size,
+            rows,
+            unit,
+        })
+    }
+    
+    move = () => {
+        
+    }
+    
+    makeAiMove = () => {
+        
+    }
+    
+    turingTest = () => {
+        
+    }
+    
+    recordGame = () => {
+        
+    }
+    
     render() {
+        let {
+            size,
+            rows,
+            unit
+        } = this.state
+        
         return (
             <div>
-                <h2>On Home</h2>
+                <Stage
+                    width={size}
+                    height={size}
+                >
+                    <Board 
+                        unit={unit}
+                        size={size}
+                        rows={rows}
+                    />
+                    {/* <Squares /> */}
+                </Stage>
             </div>
         )
     }
